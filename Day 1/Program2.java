bee51eabb5545189f4bc6af4e34303a87e0b7be6
Ashahad07 <ashahadshaikh0007@gmail.com>
2024-08-26 17:47:45 +0530

    Day 1

---
+++ Day 1/Program2.java
@@ -0,0 +1,13 @@
+class Program2{
+	public static void main(String[] args){
+		int num = 3;
+		
+		if(num % 2 == 0){
+			System.out.println("Number is Even");
+		}
+		else{
+			System.out.println("Number is Odd");
+		}
+	}
+
+}
---
+++ Day 1/Program4.java
@@ -0,0 +1,7 @@
+class Program4{
+	public static void main(String[] args){
+		for(int i = 15; i>=0; i--){
+			System.out.println(i);
+		}
+	}
+}
