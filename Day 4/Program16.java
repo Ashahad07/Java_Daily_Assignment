2d444a7b264ac1fd9bb90a5d4b12be9792972c3e
Ashahad07 <ashahadshaikh0007@gmail.com>
2024-08-29 20:06:15 +0530

    Day 4

---
+++ Day 4/Program16.java
@@ -0,0 +1,9 @@
+class Program16{
+	public static void main(String[] args){
+		for(char c = 'A'; c <= 'I'; c+=2){
+			System.out.print(c + " ");
+		}
+		
+	}
+
+}
---
+++ Day 4/Program20.java
@@ -0,0 +1,11 @@
+class Program20{
+	public static void main(String[] args){
+		for(int i = 1; i <= 4; i++){
+			for(int j = 1; j <= 4; j++){
+				System.out.print(j % 2 + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
