9a38d0471dfcb29264b6e9ecc4876851b0a98c62
Ashahad07 <ashahadshaikh0007@gmail.com>
2024-08-30 22:25:46 +0530

    Day 4

--- Day 4/Program18.java
+++ Day 4/Program18.java
@@ -1,6 +1,6 @@
 class Program18{
 	public static void main(String[] args){
-		for(int i = 0; i < 4; i++){
+		for(int i = 0; i < 3; i++){
 			for(int j = 0; j < 4; j++){
 				System.out.print("*" + " ");
 			}
