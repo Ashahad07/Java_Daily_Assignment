099f6435c0fc043155369b94d64936bb004e8367
Ashahad07 <ashahadshaikh0007@gmail.com>
2024-09-02 19:30:52 +0530

    Day 5

--- Day 5/Program22.java
+++ Day 5/Program22.java
@@ -1,21 +1,18 @@
 class Program22{
 	public static void main(String[]args){
 
-		int a=20;
-		int b=30;
-		int c=40;
-		if (a<b && a<c){
+		int a = 20;
+		int b = 30;
+		int c = 40;
+
+		if (a < b && a < c){
 			System.out.println(a+" is a smallest Number");
-		}else if (b<a && b<c){
+		}
+		else if (b < a && b < c){
 			System.out.println(b+" is a smallest number");
 		}
 		else{ 
 			System.out.println(c+" is a smallest number");
-		
-}
-	
-}
-	
-
-
+		}
+	}
 }
--- Day 5/Program23.java
+++ Day 5/Program23.java
@@ -2,22 +2,15 @@ class Program23 {
 
 	public static void main(String[]args){
 	
-	char ch = 'a';
-	char ch1 = 'b';
+		char ch = 'a';
+		char ch1 = 'b';
 	
-	if (ch=='a'||ch== 'e'||ch=='i'||ch=='o'||ch=='u'){	
-
-		System.out.println(ch+ " is vowel");
-	}else{
-		System.out.println(ch+ " is consonant");
-
+		if (ch=='a'||ch== 'e'||ch=='i'||ch=='o'||ch=='u'){	
+			System.out.println(ch+ " is vowel");
+		}	
+		else{
+			System.out.println(ch+ " is consonant");
+		}
+	
 	}
-	
-	
-
-}
-	
-
-
-
 }
