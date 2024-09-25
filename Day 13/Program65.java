ffbdbfbcd60fd4786c145dc40162067dfb56786a
Ashahad07 <ashahadshaikh0007@gmail.com>
2024-09-25 17:54:26 +0530

    Day 13

---
+++ Day 13/Program65.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Program65{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Min value");
+        int min = sc.nextInt();	
+
+	System.out.println("Enter Max value");
+	int max = sc.nextInt();
+
+        for (int i = min; i <= max; i+=2) {
+            System.out.println(i); 
+        }
+    }
+}
